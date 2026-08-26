25910389681ad8a0bf25de821cb7f87debd4ad14
Avik3-hub <sergey.nagibin55@gmail.com>
2026-08-26 10:14:36 +0300

    Create FlightData.kt

---
+++ app/src/main/java/com/example/avans/FlightData.kt
@@ -0,0 +1,9 @@
+package com.example.avans
+
+data class FlightData(
+    val departureDate: String = "",
+    val arrivalDate: String = "",
+    val fromLocation: String = "",
+    val toLocation: String = "",
+    val flightTask: String = ""
+)
