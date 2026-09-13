1c5e5216e970b465b2bf0a3410981eee96e6fb6d
Avik3-hub <sergey.nagibin55@gmail.com>
2026-09-13 14:33:48 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.avans"
         minSdk = 26
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.1.2"
+        versionCode = 4
+        versionName = "1.1.3"
 
         vectorDrawables {
             useSupportLibrary = true
